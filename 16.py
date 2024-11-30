d747db27cdf843c5d53c6d63c917b6f378630246
Dimonka2006 <166053512+Dimonka2006@users.noreply.github.com>
2024-11-30 16:00:42 +0500

    reshenie 16

--- 16.py
+++ 16.py
@@ -1,6 +1,6 @@
 # Даны два файла, 16_1.txt и 16_2.txt
 # Обьеденить их содержимое в in1.txt
-with open('16_1.txt', 'a', encoding='utf-8') as f16_1, open('16_2.txt', 'a', encoding='utf-8') as f16_2:
+with open('16_1.txt', 'a', encoding='utf-8') as f16_1, open('16_2.txt', 'r', encoding='utf-8') as f16_2:
     f16_1.write('\n')
     for line in f16_2:
         f16_1.write(line)    
