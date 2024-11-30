f990892126311146324d318ea863d842ec86b748
Dimonka2006 <166053512+Dimonka2006@users.noreply.github.com>
2024-11-30 17:01:50 +0500

    proverka

---
+++ proverka1.py
@@ -0,0 +1,16 @@
+logs = 'May 24 12:48:31 ideapad systemd[1]: logrotate.service: Succeeded.'
+data_time = logs.split()
+data_time_pr = ' '.join(data_time[0:3])
+print('Дата и время:', data_time_pr)
+
+logs = 'May 24 12:48:31 ideapad systemd[1]: logrotate.service: Succeeded.'
+service = logs.split()
+service_systemd = service[4].split(':')[0]
+print('Название сервиса:', service_systemd )
+
+
+logs = 'May 24 12:48:31 ideapad systemd[1]: logrotate.service: Succeeded.'
+logs_new = logs.replace('ideapad', 'PC-12092')
+print(logs_new)
+
+
